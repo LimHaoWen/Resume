9f998a2f8a9b7e85113b4f56748bcfa34a266de1
LimHaoWen <limhaowen_97@hotmail.sg>
2023-09-11 12:28:10 +0800

    Footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div id="contactme" className="w-full h-[40vh] relative mx-auto bg-white z-10">
+      <div id="contactme" className="w-full h-[40vh] relative mx-autos mb-24 md:mb-0 bg-white z-10">
           <div className="w-3/4 mt-10 lg:mt-36 mx-auto content-evenly">
             <div className="lg:w-1/3 block lg:inline-block font-thin text-center">
               <p className="lg:mb-6 text-xl md:text-3xl">Email</p>
