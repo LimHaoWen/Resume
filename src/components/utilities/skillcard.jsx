5807c2dc56962974cf12ed1fd5cc9698d8c836d9
LimHaoWen <limhaowen_97@hotmail.sg>
2024-11-12 02:54:04 +0800

    Update project and skill section

--- src/components/utilities/skillcard.jsx
+++ src/components/utilities/skillcard.jsx
@@ -6,8 +6,8 @@ const Skillcard = ({ src, alt, customWidth }) => {
 
     return(
         <>
-        <div className="w-1/3 md:h-64 lg:h-80 inline-block">
-            <img className={` ${cardWidth} h-auto mt-20 mb-0 mx-auto hover:scale-[1.3] reveal relative transform 
+        <div className="w-1/3 md:h-64 lg:h-80 inline-flex items-center justify-center">
+            <img className={` ${cardWidth} h-auto mt-20 hover:scale-[1.3] reveal relative transform 
             translate-y-[100px] opacity-0 transition-all duration-[1500ms] ease-out [&.active]:translate-y-0 
             [&.active]:opacity-100`} src={ src } alt={ alt } />
         </div>
