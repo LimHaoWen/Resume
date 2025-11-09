cbbc6fd52353f9a312bfdd82a6214bb55190af9b
LimHaoWen <limhaowen_97@hotmail.sg>
2025-11-09 14:47:33 +0800

    Fix mobile view for projects

--- src/components/cards/projectcard.jsx
+++ src/components/cards/projectcard.jsx
@@ -2,7 +2,7 @@ const Projectcard = ({ src, link, description }) => {
     return (
         <>
         <div className="card group relative flex justify-center w-[55vmin] h-[75vmin] lg:w-[55vmin] lg:h-[65vmin] xl:w-[35vmin] xl:h-[50vmin] max-lg:bg-darkgray hover:bg-darkgray">
-            <img className="image w-[55vmin] h-[75vmin] lg:w-[55vmin] lg:h-[65vmin] xl:w-[35vmin] xl:h-[50vmin] object-cover object-right 
+            <img className="image w-[55vmin] h-[75vmin] lg:w-[55vmin] lg:h-[65vmin] xl:w-[35vmin] xl:h-[50vmin] object-cover object-right `
             max-lg:opacity-50 group-hover:opacity-50" draggable="false" src={ src } alt="project" />
             <p className="absolute px-8 bottom-[70%] md:bottom-3/4 invisible max-lg:visible text-lightgray text-center font-medium text-sm md:text-xl group-hover:visible">{description}</p>
             <a className="project absolute bottom-1/2 left-[30%] invisible max-lg:visible md:left-[35.5%] lg:left-[34.5%] xl:left-[35.5%]
