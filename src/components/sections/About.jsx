3bf17136931acebd4d7ddfa1797dbd73298114bf
LimHaoWen <limhaowen_97@hotmail.sg>
2025-11-09 14:56:28 +0800

    Fix tablet about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -12,12 +12,12 @@ const About = () => {
 
   return (
     <>
-      <div className="relative flex md:h-[125vh] lg:h-screen xl:h-[80vh] w-full bg-transparent z-10 mt-[150px]">
-        <div className="flex flex-col lg:flex-row lg:w-[68%] xl:w-[72%] lg:flex mx-auto bg-transparent">
+      <div className="relative flex md:h-screen xl:h-[80vh] w-full bg-transparent z-10 mt-[150px]">
+        <div className="flex flex-col md:flex-row lg:w-[68%] xl:w-[72%] mx-auto bg-transparent">
           <div className="lg:w-1/2 lg:float-left">
             <img className="lg:w-10/12 lg:mt-10" src={Profile} alt="ProfilePicture"/>
           </div>
-          <div className={`relative lg:w-1/2 lg:float-right max-lg:mt-10 transform transition-all duration-1000 ease-out ${isActive ? 'translate-y-0 opacity-100' : 'translate-y-[100px] opacity-0'}`}>
+          <div className={`relative lg:w-1/2 lg:float-right max-md:mt-10 max-lg:mt-64 transform transition-all duration-1000 ease-out ${isActive ? 'translate-y-0 opacity-100' : 'translate-y-[100px] opacity-0'}`}>
             <p className="w-11/12 text-3xl md:text-6xl mx-auto text-center lg:text-left lg:float-right mb-2 lg:mb-8 lg:mt-72 dark:text-white">Hi, I'm Hao Wen</p>
             <p className="w-11/12 text-md md:text-2xl lg:text-lg mx-auto 
             text-center lg:text-left lg:float-right text-darkgray dark:text-gray">{intro}</p>
